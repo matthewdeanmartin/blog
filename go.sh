0f694cfa588b25372e3f7916260d66690fdcc290
Matthew Martin <matthewdeanmartin@gmail.com>
2023-01-08 20:26:59 -0500

    comments

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
-THEME=themes/pelican-hyde
-pelican content -s pelicanconf.py -t $THEME
+export THEME=themes/pelican-hyde
+pipenv run pelican content -s pelicanconf.py -t $THEME
 start output/index.html
