df761f002ddbfbebadeaa271fae9fef80093bb57
Matthew Martin <matthewdeanmartin@gmail.com>
2023-01-15 13:06:01 -0500

    need 2 line

---
+++ cloudflare.sh
@@ -0,0 +1,5 @@
+python --version
+source /opt/buildhome/repo/.venv/bin/activate
+pipenv --version
+/opt/buildhome/repo/.venv/bin/pelican --version
+/opt/buildhome/repo/.venv/bin/pelican content -s pelicanconf.py -t themes/pelican-hyde
