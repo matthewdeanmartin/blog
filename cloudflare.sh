c8d3a22b024bc12c1df959c90cd92a4b3981f08f
Matthew Martin <matthewdeanmartin@gmail.com>
2023-01-15 13:19:57 -0500

    again

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,9 +1,24 @@
+echo "Gather Info"
 which python
-which pip
 python --version
-source /opt/buildhome/repo/.venv/bin/activate
+echo
+which pip
 pip --version
 pip freeze
-# pipenv --version
+echo
+pip install pipenv
+which pipenv
+echo
+echo "Activate"
+echo
+source /opt/buildhome/repo/.venv/bin/activate
+echo
+echo "Did we activate?"
+echo
+pip --version
+pip freeze
+echo
+echo "Summon the pelican!!!"
+echo
 /opt/buildhome/repo/.venv/bin/pelican --version
 /opt/buildhome/repo/.venv/bin/pelican content -s pelicanconf.py -t themes/pelican-hyde
