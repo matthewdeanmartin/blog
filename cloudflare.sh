52bb9d45ee4c08f048cf2d9b358aaae13ba7cc0b
Matthew Martin <matthewdeanmartin@gmail.com>
2023-01-15 13:13:47 -0500

    why is this so hard

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,9 @@
+which python
+which pip
 python --version
 source /opt/buildhome/repo/.venv/bin/activate
-pipenv --version
+pip --version
+pip freeze
+# pipenv --version
 /opt/buildhome/repo/.venv/bin/pelican --version
 /opt/buildhome/repo/.venv/bin/pelican content -s pelicanconf.py -t themes/pelican-hyde
